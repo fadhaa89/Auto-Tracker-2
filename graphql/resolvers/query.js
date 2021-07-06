98cf9c8262a3b918cc8e2907bf2322790acc5153
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-07-05 21:25:35 -0500

    query.js is modified and updated

---
+++ graphql/resolvers/query.js
@@ -0,0 +1,30 @@
+const { Vehicle, User } = require("../../models");
+
+const vehiclesVar = async (vehiclesId) => {
+    try{
+        const vehicles2 = await Vehicle.find({_id: {$in:vehiclesId}});
+
+        return vehicles2.map(vehicle => ({
+            ...vehicle._doc,
+            user: userVar.bind(this, vehicle._doc.user)
+        }))
+    }
+    catch {
+        throw err
+    }
+    
+}
+
+const userVar = async (userId) => {
+    try{
+        const user2 = await User.findById(userId);
+        
+        return {
+            ...user2._doc,
+            vehicles: vehiclesVar.bind(this, user2._doc.vehicles)
+        }
+    }
+    catch {
+        throw err
+    }        
+}
