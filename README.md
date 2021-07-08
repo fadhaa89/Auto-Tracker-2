50e30621cb41a44cff35b28c63d18a0fc8bdc93e
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-07-08 18:18:20 -0500

    app.js is modified -error fixed and updated

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Auto-Tracker-2
+
+# description:Auto-Tracker Application
+this is an auto tracking system , where users can register their vehicle , view the list of their vehicle and drivers , also can do sign in and sign out
+
+# Technologies Used
+GraphQL
+Apollo Server
+Express.JS, NodeJS
+indexDB
+MongoDB
+Redux
+React
+Stripe
+Materials UI
+Deployment on Heroku
+
+# Authors :
+Group of two project ; 01-Fadhaa Abdullah . 02-Mina Mahmood .
+
+July-2021.
+
+# Links :
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-mport '../style/App.css';
-//, useQuery, gql,
+import '../style/App.css';
+
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+//import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import GetVeicles from './GetVehicles';
+import GetDrivers from './GetDrivers';
 import Register from './SignUp';
 import Login from './Login';
 
@@ -11,8 +13,18 @@ const link = from([
   new HttpLink({uri: "http://localhost:4000/api"})
 ]);
 
+const token = localStorage.getItem('AUTH_TOKEN'); 
+
+/*const authLink = setContext((_, { headers }) => {  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});*/
+
 const client = new ApolloClient({
-  //uri: 'http://localhost:4000/api',
   cache: new InMemoryCache(),
   link: link
 });
@@ -21,9 +33,10 @@ const client = new ApolloClient({
 function App() {
   return <Router>
     <ApolloProvider client={client}>
-    <Route path="/sign-up" component={ Register } />
-    <Route path="/vehicle" component={ GetVeicles } />
+    <Route path="/sign-up"  component={ Register } />
     <Route path="/login" component={ Login } />
+    <Route path="/vehicle" render = { () => token ? (<GetVeicles/>) : (<Redirect to="/login"/>)} />
+    <Route path="/driver" render = { () => token ? (<GetDrivers/>) : (<Redirect to="/login"/>)} />
     </ApolloProvider>
     </Router>;
 }
