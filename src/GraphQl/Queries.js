22eec1f3c53c3047dad200d838c1ee3d9f219d86
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-07-08 18:30:08 -0500

    gql is updated with LOAD_USERS

--- src/GraphQl/Queries.js
+++ src/GraphQl/Queries.js
@@ -24,3 +24,29 @@ export const LOAD_VEHICLS = gql`
         }
   }
 `
+
+export const LOAD_USERS = gql`
+    query {
+        users{
+            firstName,
+            lastName,
+            email,
+            address,
+            _id,
+            vehicles{
+                year,
+                model,
+                color,
+                vin,
+                license_plate,
+                toll_tag_number,
+                insurance_expire,
+                registration_expire,
+                purchase_mileage,
+                oil_change_mileage,
+                tire_change_mileage,
+                _id
+            }
+        }
+    }
+`
