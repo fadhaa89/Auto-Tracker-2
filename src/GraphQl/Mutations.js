5fbbf7a229fbb2b5a401721521e1ad2253628905
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-07-08 18:31:44 -0500

    application is updated with DeleteVehicle facility and UpdateVehicle as it was in sign in and sing out

--- src/GraphQl/Mutations.js
+++ src/GraphQl/Mutations.js
@@ -28,20 +28,113 @@ export const SIGN_UP = gql`
 `
 
 export const SIGN_IN = gql`
-    mutation SignUp(
+    mutation SignIn(
             $email: String! 
             $password: String! 
         ){
-        SignUp(
-            email: $email
-            password: $password
-        ){
-            firstName,
-            lastName,
-            email,
-            address,
-            _id,
-            token   
-        }
+            SignIn(
+                email: $email
+                password: $password
+            ){
+                firstName,
+                lastName,
+                email,
+                address,
+                _id,
+                token   
+            }
   }
+`
+
+export const CREATE_VEHICLE = gql`
+    mutation createVehicle( 
+            $year: String! 
+            $model: String! 
+            $color: String! 
+            $vin: String! 
+            $license_plate: String! 
+            $toll_tag_number: String! 
+            $insurance_expire:  String! 
+            $registration_expire: String! 
+            $purchase_mileage: String! 
+            $oil_change_mileage: String! 
+            $tire_change_mileage: String! 
+            $user: String! 
+        ){
+            createVehicle( 
+                year: $year 
+                model: $model 
+                color: $color 
+                vin: $vin 
+                license_plate: $license_plate 
+                toll_tag_number: $toll_tag_number 
+                insurance_expire:  $insurance_expire 
+                registration_expire: $registration_expire 
+                purchase_mileage: $purchase_mileage 
+                oil_change_mileage: $oil_change_mileage 
+                tire_change_mileage:  $tire_change_mileage
+                user: $user 
+                ){
+                    year,
+                    model,
+                    color,
+                    vin,
+                    _id,
+                    license_plate
+                }
+    }
+`
+
+export const UPDATE_VEHICLE = gql`
+    mutation UpdateVehicle( 
+            $id: ID!
+            $year: String! 
+            $model: String! 
+            $color: String! 
+            $vin: String! 
+            $license_plate: String! 
+            $toll_tag_number: String! 
+            $insurance_expire:  String! 
+            $registration_expire: String! 
+            $purchase_mileage: String! 
+            $oil_change_mileage: String! 
+            $tire_change_mileage: String! 
+            $user: String! 
+        ){
+            UpdateVehicle( 
+                id: $id
+                year: $year 
+                model: $model 
+                color: $color 
+                vin: $vin 
+                license_plate: $license_plate 
+                toll_tag_number: $toll_tag_number 
+                insurance_expire:  $insurance_expire 
+                registration_expire: $registration_expire 
+                purchase_mileage: $purchase_mileage 
+                oil_change_mileage: $oil_change_mileage 
+                tire_change_mileage:  $tire_change_mileage
+                user: $user 
+                ){
+                    year,
+                    model,
+                    color,
+                    vin,
+                    _id,
+                    license_plate
+                }
+    }
+`
+
+export const DELETE_VEHICLE = gql`
+    mutation DeleteVehicle(
+        $id: ID!
+        ){
+            DeleteVehicle(
+                id: $id
+            ){
+                model,
+                _id
+            }
+        }
 `
