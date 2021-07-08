5d979413fb78243fc0520d31bf824eb4d3f85918
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-07-08 18:40:33 -0500

    application data is updated and eddited

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -37,9 +37,13 @@ module.exports = gql`
 
     type Mutation {
         SignUp(firstName: String!,  lastName: String!, email: String!, password: String!, confirmPassword: String!, address: String!) : User 
+
         SignIn( email: String!, password: String!) : User 
+
         UpdateUser(id: ID!, name: String!, email: String!) : User
+
         DeleteUser(id: ID!): Boolean!,
+
         createVehicle(year: String!,
             model: String!,
             color: String!,
@@ -52,5 +56,22 @@ module.exports = gql`
             oil_change_mileage: String!,
             tire_change_mileage: String!,
             user: String!): Vehicle
+
+        UpdateVehicle(
+            id: ID!,
+            year: String!,
+            model: String!,
+            color: String!,
+            vin: String!,
+            license_plate: String!,
+            toll_tag_number: String!,
+            insurance_expire: String!,
+            registration_expire: String!,
+            purchase_mileage: String!,
+            oil_change_mileage: String!,
+            tire_change_mileage: String!,
+            user: String!): Vehicle
+
+        DeleteVehicle(id: ID!): Vehicle,
     }
 `;
